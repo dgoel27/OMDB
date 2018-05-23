af2bf62265fd15b17e2dbdaa045b36044434a7eb
Divya <dgoel27@yahoo.in>
2018-05-23 23:13:25 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // main document ready function to check if dom is loaded fully or not
 
-let omdbToken, token_url;
+let omdbToken, token_url, eachElement;
 
 $(document).ready(() => {
 
-    let var_id, src_split, final_img, img_src, criteriaFlag = 0, search_criteria, timer = null, lastValue = '';
+    let var_id, src_split, final_img, img_src, criteriaFlag = 0, search_criteria, timer = null, lastValue = '', alertMsg = '';
 
     $("#dropdown-menu1 a.dropdown-item").click(function() {
 
@@ -33,7 +33,10 @@ $(document).ready(() => {
     });
 
     $("#dropdown-menu2 a.dropdown-item").click(function() {
-        $('#search-button').removeClass('d-none');       
+        $('#search-button').removeClass('d-none');
+        $('#imdbID').val('');
+        $('#movieTitle').val('');
+        $('#movieYear').val('');       
 
         if($(this).text() == 'IMDb ID') {
             $("#imdbID").removeClass('d-none');
@@ -51,22 +54,7 @@ $(document).ready(() => {
             document.getElementById('navbarDropdown2').innerHTML = 'Criteria';
             document.getElementById('navbarDropdown').innerHTML = 'Genre';
         }
-    })   
-
-    // $('.form-control').keydown(function() {
-    //     clearTimeout(timer);
-    //     timer = setTimeout( function() {
-    //         if(criteriaFlag == 1) {
-    //              search_criteria = document.getElementById('imdbID').value;
-    //         } else {
-    //             search_criteria = document.getElementById('imdbID').value;
-    //         }  
-    //         if($(this).val != '' && $(this).val != lastValue){
-    //             token_url = 'https://www.omdbapi.com/?apikey=ca2cf604&i=' + search_criteria;
-    //             getAllDetails();
-    //         }
-    //     }, 500);
-    // });    
+    })       
 
     $('.form-control').mouseout(function() {
 
@@ -79,6 +67,12 @@ $(document).ready(() => {
         if($(this).val() != '' && $(this).val() != lastValue){
             lastValue = $(this).val();
             token_url = 'https://www.omdbapi.com/?apikey=ca2cf604&i=' + search_criteria;
+            document.getElementById('movieSearchModalLabel').innerHTML = '';
+            document.getElementById('desc').innerHTML = '';
+            document.getElementById('imdb').innerHTML = '';
+            document.getElementById('plot').innerHTML = '';
+            document.getElementById('rotten-tomatoes').innerHTML = '';
+            document.getElementById('metacritic').innerHTML = '';
             getAllDetails();
         }
 
@@ -141,11 +135,56 @@ $(document).ready(() => {
         $('.container-fluid').css('opacity','1');
     })    
 
-    //$('#contactModal .modal-body .contact')[0]
+    $('[name="name"]').change(() => {
+        if(!(/[a-zA-Z]+$/.test($('[name="name"]').val()))) {
+            alert("You have entered an invalid name!");
+            $('[name="emailid"]').attr('disabled','disabled');      
+            $('#contactModal .modal-body textarea').attr('disabled','disabled');      
+        } else {
+            $('[name="emailid"]').removeAttr('disabled'); 
+            $('#contactModal .modal-body textarea').removeAttr('disabled');
+        }       
+    })    
+
+    function validateEmail(email) {
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        return emailReg.test(email);
+    }
+
+    $('[name="emailid"]').change(() => {
+        if( !validateEmail($('[name="emailid"]').val()) || $('[name="emailid"]').val() == '') {
+            alert("You have entered an invalid email address!");
+            $('#contactModal .modal-body textarea').attr('disabled','disabled');
+            $('[name="name"]').attr('disabled','disabled');
+        } else {
+            $('#contactModal .modal-body textarea').removeAttr('disabled');
+            $('[name="name"]').removeAttr('disabled'); 
+        }
+    })
+
+    $('#contactModal .modal-body textarea').mouseout(() => {
+        if($('#contactModal .modal-body textarea').val() == '') {
+            $('#contactModal .modal-footer button').attr('disabled','disabled');            
+        } else {
+            $('#contactModal .modal-footer button').removeAttr('disabled'); 
+        }
+    })
 
     $('#contactModal .modal-footer button').click(() => {
-        alert("Your message has been submitted..!!");
+        alertMsg = "Your message has been submitted..!!";
     })
+
+    $('#contactModal .close').click(() => {
+        alertMsg = '';
+    })
+
+    $("#contactModal").on("hidden.bs.modal", function () {
+        if(alertMsg == "Your message has been submitted..!!" && $('#contactModal .modal-body textarea').val() != '') {
+            alert(alertMsg);
+        }
+        $('#contactModal .modal-body textarea').val('');
+        $('#contactModal .modal-body input').val('');
+    });    
 
     // omdbToken = prompt("Please enter your OMDb Token:", "");
     // if (omdbToken == null || omdbToken == "") {
@@ -170,6 +209,7 @@ let getAllDetails = () => {
         success: (response) => {
 
             console.log(response);
+
             if(response.Error != '' && response.Error != undefined) {
                 $('#search-button').attr('disabled','disabled');
                 alert(response.Error);
@@ -180,28 +220,96 @@ let getAllDetails = () => {
             document.getElementById('movieSearchModalLabel').innerHTML = response.Title 
             + "&nbsp;<span class=\"text-warning\" style=\"font-weight: lighter;font-size: 20px;\">(" + response.Year + ")</span>";   
 
-            document.getElementById('desc').innerHTML = "<small>" + response.Rated + "&nbsp;&nbsp;|&nbsp;&nbsp;" +
-            response.Runtime + "&nbsp;&nbsp;|&nbsp;&nbsp;" + response.Genre + "&nbsp;&nbsp;|&nbsp;&nbsp;" + response.Released + 
-            "&nbsp;(" + response.Country + ")&nbsp;&nbsp;|&nbsp;&nbsp;" + response.Language + "</small>";  
+            if(response.Rated != '' && response.Rated != undefined && response.Rated != 'N/A') {
+                document.getElementById('desc').innerHTML = "<small>" + response.Rated + "&nbsp;&nbsp;|&nbsp;&nbsp;</small>";
+            }
 
-            if(response.Poster == '' || response.Poster == undefined) {
+            if(response.Runtime != '' && response.Runtime != undefined && response.Runtime != 'N/A') {
+                $('#desc').append("<small>" + response.Runtime + "&nbsp;&nbsp;|&nbsp;&nbsp;</small>");
+            }
+
+            if(response.Genre != '' && response.Genre != undefined && response.Genre != 'N/A') {
+                $('#desc').append("<small>" + response.Genre + "&nbsp;&nbsp;|&nbsp;&nbsp;</small>");
+            }
+
+            if(response.Released != '' && response.Released != undefined && response.Released != 'N/A') {
+                if(response.Country != '' && response.Country != undefined && response.Country != 'N/A') {
+                    $('#desc').append("<small>" + response.Released + "</small>");
+                } else {
+                    $('#desc').append("<small>" + response.Released + "&nbsp;&nbsp;|&nbsp;&nbsp;</small>");
+                }
+            }
+
+            if(response.Country != '' && response.Country != undefined && response.Country != 'N/A') {
+                if(response.Released != '' && response.Released != undefined && response.Released != 'N/A') {
+                    $('#desc').append("<small>&nbsp;(" + response.Country + ")&nbsp;&nbsp;|&nbsp;&nbsp;</small>");                    
+                } else {
+                    $('#desc').append("<small>" + response.Country + "&nbsp;&nbsp;|&nbsp;&nbsp;</small>");
+                }                
+            }
+
+            if(response.Language != '' && response.Language != undefined && response.Language != 'N/A') {
+                $('#desc').append("<small>" + response.Language + "</small>");
+            }
+
+            if(response.Poster == '' || response.Poster == undefined || response.Poster == 'N/A') {
                 $('#poster').html('<img src="no-image.png" class="img-fluid"/>');       
             } else {
                 $('#poster').html('<img src="' + response.Poster + '" class="img-fluid"/>');
             }
 
-            document.getElementById('imdb').innerHTML = "<b>" + response.imdbRating + "</b><small style=\"font-size:10px;\">/10<br>" 
-            + response.imdbVotes + "<small>";
+            if(response.imdbRating != undefined && response.imdbRating != 'N/A' && response.imdbRating != '') {
+                document.getElementById('imdb').innerHTML = "<b>" + response.imdbRating + "</b><small style=\"font-size:10px;\">/10<br>" 
+                + response.imdbVotes + "<small>";
+            }
 
-            $('#rotten-tomatoes').append(response.Ratings[1].Value);
+            if(response.Ratings[1] != undefined && response.Ratings[1] != 'N/A' && response.Ratings[1] != '') { 
+                $('#rotten-tomatoes').append(response.Ratings[1].Value);
+            }
 
-            $('#metacritic').append(response.Metascore + "%");
+            if(response.Metascore != undefined && response.Metascore != 'N/A' && response.Metascore != '') {
+                $('#metacritic').append(response.Metascore + "%");
+            }
 
-            document.getElementById('plot').innerHTML = "<small><b>" + response.Plot + "</b><br><br><b>Director : </b>" + 
-            response.Director + "<br><b>Actors : </b>" + response.Actors + "<br><b>Writer : </b>" + response.Writer + 
-            "<br><b>Awards : </b>" + response.Awards + "<br><b>DVD : </b>" + response.DVD + "<br><b>Website : </b><a href=\"" 
-            + response.Website + "\">" + response.Website + "</a><br><b>Production : </b>" 
-            + response.Production + "<br><b>BoxOffice : </b>" + response.BoxOffice + "</small>";
+            if(response.Plot != undefined && response.Plot != 'N/A' && response.Plot != '') {
+                document.getElementById('plot').innerHTML = "<small><b>" + response.Plot + "</b></small><br>";
+            }
+
+            if(response.Director != undefined && response.Director != 'N/A' && response.Director != '') {
+                $('#plot').append("<br><small><b>Director : </b>" + response.Director + "</small>");
+            }
+
+            if(response.Actors != undefined && response.Actors != 'N/A' && response.Actors != '') {
+                $('#plot').append("<br><small><b>Actors : </b>" + response.Actors + "</small>");
+            }
+
+            if(response.Writer != undefined && response.Writer != 'N/A' && response.Writer != '') {
+                $('#plot').append("<br><small><b>Writer : </b>" + response.Writer + "</small>");
+            }
+
+            if(response.Awards != undefined && response.Awards != 'N/A' && response.Awards != '') {
+                $('#plot').append("<br><small><b>Awards : </b>" + response.Awards + "</small>");
+            }
+
+            if(response.Type != undefined && response.Type != 'N/A' && response.Type != '') {
+                $('#plot').append("<br><small><b>Type : </b>" + response.Type + "</small>");
+            }
+
+            if(response.DVD != undefined && response.DVD != 'N/A' && response.DVD != '') {
+                $('#plot').append("<br><small><b>DVD : </b>" + response.DVD + "</small>");
+            }
+
+            if(response.Website != undefined && response.Website != 'N/A' && response.Website != '') {
+                $('#plot').append("<br><small><b>Website : </b><a href=\"" + response.Website + "\">" + response.Website + "</small>");
+            }
+
+            if(response.Production != undefined && response.Production != 'N/A' && response.Production != '') {
+                $('#plot').append("</a><small><br><b>Production : </b>" + response.Production + "</small>");
+            }
+
+            if(response.BoxOffice != undefined && response.BoxOffice != 'N/A' && response.BoxOffice != '') {
+                $('#plot').append("<br><small><b>BoxOffice : </b>" + response.BoxOffice + "</small>");
+            }
 
         }, 
             error: (err) => {
